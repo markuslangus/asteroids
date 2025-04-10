7783f8a08c5f77c3d79a7689781959be205ce2c6
markuslangus <markus.fabian.lang@gmail.com>
2025-04-10 22:36:34 +0200

    Add function to draw the player

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 # the open-source pygame library
 # throughout this file
 import pygame
+from player import Player
 from constants import *
 
 
@@ -12,12 +13,22 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    clock = pygame.time.Clock()
+    dt = 0
+
+    half_width = SCREEN_WIDTH / 2
+    half_height = SCREEN_HEIGHT / 2
+    player = Player(half_width, half_height, PLAYER_RADIUS)
+
+    # game loop
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
